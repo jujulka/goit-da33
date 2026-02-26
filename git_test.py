51c348baa476adfb5950628f34a55826f193a836
jujulka <yulia.kostynyuk@gmail.com>
2026-02-26 22:44:08 +0200

    added age into the features

--- git_test.py
+++ git_test.py
@@ -3,4 +3,7 @@ name = input('Enter your name')
 print(f'Helloooo-hello-hi {name}')
 
 height = input('Enter your height')
-print('Your height is {height}')
+print('Your height is {height}' meters)
+
+age = input ('Enter your age')
+print(f'Your age is {age}' years)
